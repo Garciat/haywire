8a6a025667417e8e70dbf109f014e86d33d9b554
Gabriel Garcia <garciat@live.com>
2017-04-01 15:27:42 -0400

    Shallow dependency clones; parallel make

--- compile_dependencies.sh
+++ compile_dependencies.sh
@@ -2,62 +2,68 @@
 
 # Getting libuv
 if [ ! -d "lib/libuv" ]; then
-    echo "git clone https://github.com/libuv/libuv.git lib/libuv"
-    git clone https://github.com/libuv/libuv.git lib/libuv
+    CMD="git clone --depth=1 https://github.com/libuv/libuv.git lib/libuv"
+    echo "$CMD"
+    $CMD
     cd lib/libuv
     sh autogen.sh
     ./configure
-    make
+    make -j
     cd ../../
 fi
 
 # Getting Wrk
 if [ ! -d "bin/wrk" ]; then
-    echo "git clone https://github.com/wg/wrk.git bin/wrk"
-    git clone https://github.com/wg/wrk.git bin/wrk
+    CMD="git clone --depth=1 https://github.com/wg/wrk.git bin/wrk"
+    echo "$CMD"
+    $CMD
     cd bin/wrk
-    make
+    make -j
     cd ../../
 fi
 
 # Getting Wrk2
 if [ ! -d "bin/wrk2" ]; then
-    echo "git clone https://github.com/giltene/wrk2.git bin/wrk2"
-    git clone https://github.com/giltene/wrk2.git bin/wrk2
+    echo "git clone --depth=1 https://github.com/giltene/wrk2.git bin/wrk2"
+    echo "$CMD"
+    $CMD
     cd bin/wrk2
-    make
+    make -j
     cd ../../
 fi
 
 # Getting libumem
 if [ ! -d "lib/libumem" ]; then
-    echo "git clone https://github.com/gburd/libumem.git lib/libumem"
-    git clone https://github.com/gburd/libumem.git lib/libumem
+    CMD="git clone --depth=1 https://github.com/gburd/libumem.git lib/libumem"
+    echo "$CMD"
+    $CMD
     cd lib/libumem
     ./autogen.sh
     ./configure
-    make
+    make -j
     cd ../../
 fi
 
 # Getting TCMalloc
 if [ ! -d "lib/gperftools" ]; then
-    echo "git clone https://github.com/gperftools/gperftools.git lib/gperftools"
-    git clone https://github.com/gperftools/gperftools.git lib/gperftools
+    CMD="git clone --depth=1 https://github.com/gperftools/gperftools.git lib/gperftools"
+    echo "$CMD"
+    $CMD
     cd lib/gperftools
     ./autogen.sh
     ./configure
-    make
+    make -j
     cd ../../
 fi
 
 # Getting JEMalloc
 if [ ! -d "lib/jemalloc" ]; then
-    echo "git clone https://github.com/jemalloc/jemalloc.git lib/jemalloc"
-    git clone https://github.com/jemalloc/jemalloc.git lib/jemalloc
+    CMD="git clone --depth=1 https://github.com/jemalloc/jemalloc.git lib/jemalloc"
+    echo "$CMD"
+    $CMD
     cd lib/gperftools
     ./autogen.sh
     ./configure
-    make
+    make -j
     cd ../../
 fi
